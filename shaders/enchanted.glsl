73bfd53e83906929a6a14a16a64a2fa94d5a8858
Coder2195Text <63069499+Coder2195Text@users.noreply.github.com>
2021-07-18 21:37:24 -0400

    Added thickness uniform

--- shaders/enchanted.glsl
+++ shaders/enchanted.glsl
@@ -2,6 +2,7 @@ varying vec2 vTextureCoord;
 
 uniform sampler2D uSampler;
 uniform float uTime;
+uniform float uThickness
 
 vec4 purple = vec4(0.5, 0.0, 0.4, 0.0);
 float interval = 400.0;
@@ -12,7 +13,7 @@ void main (void)
 
   float a = (
       sin(
-        (gl_FragCoord.x * 0.2 + gl_FragCoord.y * 0.2 - uTime * 100.0) * 0.1
+        (gl_FragCoord.x / uThickness + gl_FragCoord.y / uThickness - uTime * 100.0) * 0.1
       ) + 3.0
     ) / 3.0;
   
